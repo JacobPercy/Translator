b17fe2a800b901efba5144b12934dbcd5c206417
Jacob Percy <jacob.percy@icloud.com>
2024-02-27 21:20:21 -0500

    added title

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import customtkinter
+import tkinter
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -6,10 +7,14 @@ customtkinter.set_default_color_theme("blue")
 app = customtkinter.CTk()
 app.geometry("400x240")
 
+app.title("Translator")
+
+
+
 def button_function():
     print("button pressed")
 
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+button = customtkinter.CTkButton(master=app, text="Launch", command=button_function)
 button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
 app.mainloop()
