e31649e579e900b086302b576a20ddb9196673f9
Jacob Percy <jacob.percy@icloud.com>
2024-03-04 17:56:19 -0500

    started on main.py

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-import customtkinter
-import tkinter
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
+class App(ctk.CTk):
+    
+    def __init__(self) -> None:
+        super().__init__()
+        self.title("Translator")
+        self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+0+0")
 
-app = customtkinter.CTk()
-app.geometry("400x240")
-
-app.title("Translator")
-
-
+        self.window_title = ctk.CTkLabel(
+            self,
+            text="Automatic Translation Tool".replace(
+                    "               ",
+                    ""
+                ),
+            font = ("JetBrains Mono", 36)
+        )
+        self.window_title.pack(pady=20)
+        button = ctk.CTkButton(master=self, text="Launch", command=button_function)
+        button.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+        self.mainloop()
 
 def button_function():
     print("button pressed")
 
-button = customtkinter.CTkButton(master=app, text="Launch", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+def main():
+    ctk.set_appearance_mode("dark")
+    App()
 
-app.mainloop()
+
+if __name__ == "__main__":
+    main()
