d923ea02c27f185a18c0ac4e0888bedb18814db0
Jacob Percy <jacob.percy@icloud.com>
2024-03-04 21:31:38 -0500

    added font sizes and types

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import customtkinter as ctk
-import tkinter
 
 class App(ctk.CTk):
     
     def __init__(self):
+        super().__init__()
+
         def clear_screen(self):
             for widget in self.winfo_children():
                 widget.destroy()
+        
+
         def button_function():
             clear_screen(self.window_title)
-        super().__init__()
+        
+
         self.title("Translator")
         self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+0+0")
 
-        self.window_title = ctk.CTkLabel(
-            self,
-            text="Automatic Translation Tool".replace(
-                    "               ",
-                    ""
-                ),
-            font = ("JetBrains Mono", 75)
-        )
+
+
+        self.window_title = ctk.CTkLabel(self,text="Translator",font = ("Calibri", 500))
         self.window_title.pack(pady=20)
-        button = ctk.CTkButton(master=self, text="Launch", command=button_function)
+        button = ctk.CTkButton(master=self, text="LAUNCH", height=200,width=800,command=button_function, font = ("Calibri", 200,'bold'))
         button.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
 
         self.mainloop()
