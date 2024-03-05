451314b20f07a21f3ce0fdd491e4da7381022bbf
Jacob Percy <jacob.percy@icloud.com>
2024-03-04 21:46:21 -0500

    Clearing page thing works

--- main.py
+++ main.py
@@ -3,26 +3,25 @@ import customtkinter as ctk
 class App(ctk.CTk):
     
     def __init__(self):
+
         super().__init__()
-
-        def clear_screen(self):
-            for widget in self.winfo_children():
-                widget.destroy()
-        
-
-        def button_function(self):
-            self.clear_screen()
-        
-
         self.title("Translator")
         self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+0+0")
 
         self.window_title = ctk.CTkLabel(self,text="Translator",font = ("Calibri", 500))
         self.window_title.pack(pady=20)
-        button = ctk.CTkButton(master=self, text="LAUNCH", height=200,width=800,command=button_function, font = ("Calibri", 200,'bold'))
+        button = ctk.CTkButton(master=self, text="LAUNCH", height=200,width=800,command=self.button_function, font = ("Calibri", 200,'bold'))
         button.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
 
         self.mainloop()
+    
+    def clear_screen(self):
+        for widget in self.winfo_children():
+            widget.destroy()
+        
+
+    def button_function(self):
+        self.clear_screen()
 
 def main():
     ctk.set_appearance_mode("dark")
